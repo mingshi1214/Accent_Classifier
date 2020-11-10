4e5fca4c8ee48ec627c8cb051d89d0f4a5b89d96
catglossop <catherine.glossop@mail.utoronto.ca>
2020-11-09 22:52:53 -0500

    update on load and convert code

--- load_data.py
+++ load_data.py
@@ -1,10 +1,19 @@
-""" This file loads and seperates out the files that are relevant for training/testing.
+""" This file loads and seperates out the files that are relevant for training/testing 
+into seperate folders and converts them to .wav file types.
 All the original data set will be stored locally while revelant files may be pushed to
 the git repo or shared via Google Drive """
 
 import os 
+import numpy as np
+import re 
+import re
+match = re.match(r"([a-z]+)([0-9]+)", 'foofo21', re.I)
 directory = os.getcwd()
-recordings = open(os.path.join(directory,"/APS360_Project_Data/archive/recordings/recordings"))
+recordings = os.path.join(directory,"APS360_Project_Data/archive/recordings/recordings/")
 
-for sample in recordings:
-    
+for sample in os.listdir(recordings):
+    match = re.match(r"([a-z]+)([0-9]+)", sample, re.I)
+    if match:
+        lang = match.groups()[0]
+        print(lang)
+    save_path = os.path.join
