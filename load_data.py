8e65e754028f866fa9c79423bdd8dc1871d09967
catglossop <catherine.glossop@mail.utoronto.ca>
2020-11-24 00:38:49 -0500

    load_data.py updated split and filter

--- load_data.py
+++ load_data.py
@@ -9,22 +9,75 @@ import numpy as np
 import re 
 import re
 import librosa
+import pandas as pd
 from shutil import copyfile
 match = re.match(r"([a-z]+)([0-9]+)", 'foofo21', re.I)
 directory = os.getcwd()
 recordings = os.path.join(directory,"APS360_Project_Data/archive/recordings/recordings/")
+csv = os.path.join(directory,"APS360_Project_Data/archive/speakers_all.csv")
+df = pd.read_csv(csv)
+subdf = df[["filename", "native_language", "country", "file_missing?"]]
+rows_not_missing = subdf.loc[subdf['file_missing?'] == False]
 
-for sample in os.listdir(recordings):
+english_samples = rows_not_missing.loc[rows_not_missing['native_language'] == 'english']
+english_samples = english_samples.loc[rows_not_missing['country'].isin(['usa','canada'])]
+english_samples = english_samples[:50]
+
+mandarin_samples = rows_not_missing.loc[rows_not_missing['native_language'] == 'mandarin']
+mandarin_samples = mandarin_samples[:50]
+
+tagalog_samples = rows_not_missing.loc[rows_not_missing['native_language'] == 'spanish']
+tagalog_samples = tagalog_samples[:50]
+
+hindi_samples = rows_not_missing.loc[rows_not_missing['country'] == 'india']
+hindi_samples = hindi_samples[:50]
+
+samples_lst = [english_samples, mandarin_samples, tagalog_samples, hindi_samples]
+samples = pd.concat(samples_lst)
+cnt = 0 
+samples = samples.sample(frac=1)
+samples = samples['filename']
+for sample in samples:
+    cnt = cnt + 1
+
+    # Get the native language of the speaker
     match = re.match(r"([a-z]+)([0-9]+)", sample, re.I)
     if match:
         lang = match.groups()[0]
-    if lang == "english" or lang == "hindi" or lang == "mandarin" or lang == "tagalog":
-        save_path = os.path.join(os.path.join(directory,"data_specific"),lang)
+    
+    # Save 70% of data to the train set  
+    if cnt/len(samples) < 0.70:
+        sample_name = sample + ".mp3"
+        dirs = [directory,"audio_split_2","train",lang]
+        save_path = os.path.join(*dirs)
         try:
-            copyfile(os.path.join(recordings,sample), os.path.join(save_path,sample))
+            copyfile(os.path.join(recordings,sample_name), os.path.join(save_path,sample_name))
         except:
             os.mkdir(save_path)
-            copyfile(os.path.join(recordings,sample), os.path.join(save_path,sample))
+            copyfile(os.path.join(recordings,sample_name), os.path.join(save_path,sample_name))
+    
+    # Save 15% of data to the validation set 
+    if cnt/len(samples) >= 0.70 and cnt/len(samples) < 0.85:
+        sample_name = sample + ".mp3"
+        dirs = [directory,"audio_split_2","validation",lang]
+        save_path = os.path.join(*dirs)
+        try:
+            copyfile(os.path.join(recordings,sample_name), os.path.join(save_path,sample_name))
+        except:
+            os.mkdir(save_path)
+            copyfile(os.path.join(recordings,sample_name), os.path.join(save_path,sample_name))
+    
+    # Save 15 % of data to the test set 
+    if cnt/len(samples) >= 0.85:
+        sample_name = sample + ".mp3"
+        dirs = [directory,"audio_split_2","test",lang]
+        save_path = os.path.join(*dirs)
+        try:
+            copyfile(os.path.join(recordings,sample_name), os.path.join(save_path,sample_name))
+        except:
+            os.mkdir(save_path)
+            copyfile(os.path.join(recordings,sample_name), os.path.join(save_path,sample_name))
+    
 
     
     
