78acfde40d74e8ae5a4003b1d0a9c2fdea20a309
catglossop <catherine.glossop@mail.utoronto.ca>
2020-12-18 19:14:11 -0500

    removing unnecessary files

--- load_data.py
+++ load_data.py
@@ -19,47 +19,53 @@ df = pd.read_csv(csv)
 subdf = df[["filename", "native_language", "country", "file_missing?"]]
 rows_not_missing = subdf.loc[subdf['file_missing?'] == False]
 
-english_samples = rows_not_missing.loc[rows_not_missing['native_language'] == 'english']
-english_samples = english_samples.loc[rows_not_missing['country'].isin(['usa','canada'])]
-english_samples = english_samples[:50]
+
+spanish_samples = rows_not_missing.loc[rows_not_missing['native_language'] == 'english']
+spanish_samples = spanish_samples.loc[rows_not_missing['country'].isin(['uk])]
+spanish_samples = spanish_samples[:60]
+print("This is the number of samples: ", len(spanish_samples))
+
+arabic_samples = rows_not_missing.loc[rows_not_missing['native_language'] == 'hindi']
+arabic_samples_2 = rows_not_missing.loc[rows_not_missing['country'] == 'india']
+arabic_samples_2 = arabic_samples_2.loc[arabic_samples_2['native_language'] != 'hindi']
+arabic_samples = pd.concat([arabic_samples, arabic_samples_2])[:60]
+print("This is the number of samples: ", len(arabic_samples))
 
 mandarin_samples = rows_not_missing.loc[rows_not_missing['native_language'] == 'mandarin']
-mandarin_samples = mandarin_samples[:50]
+mandarin_samples = mandarin_samples[:60]
+print("This is the number of samples: ", len(mandarin_samples))
 
-tagalog_samples = rows_not_missing.loc[rows_not_missing['native_language'] == 'spanish']
-tagalog_samples = tagalog_samples[:50]
-
-hindi_samples = rows_not_missing.loc[rows_not_missing['country'] == 'india']
-hindi_samples = hindi_samples[:50]
-
-samples_lst = [english_samples, mandarin_samples, tagalog_samples, hindi_samples]
+samples_lst = 
+#samples_lst = [spanish_samples, arabic_samples, mandarin_samples]
 samples = pd.concat(samples_lst)
 cnt = 0 
 samples = samples.sample(frac=1)
 samples = samples['filename']
+print("Loading 3 class split...")
 for sample in samples:
     cnt = cnt + 1
-
     # Get the native language of the speaker
     match = re.match(r"([a-z]+)([0-9]+)", sample, re.I)
     if match:
         lang = match.groups()[0]
-    
+    if lang != "mandarin" and lang != "english":
+        lang = "india"
+    print(lang)
     # Save 70% of data to the train set  
-    if cnt/len(samples) < 0.70:
+    if cnt/len(samples) < 0.75:
         sample_name = sample + ".mp3"
-        dirs = [directory,"audio_split_2","train",lang]
+        dirs = [directory,"audio_split_EMI","train",lang]
         save_path = os.path.join(*dirs)
-        try:
+        try: 
             copyfile(os.path.join(recordings,sample_name), os.path.join(save_path,sample_name))
         except:
             os.mkdir(save_path)
             copyfile(os.path.join(recordings,sample_name), os.path.join(save_path,sample_name))
     
     # Save 15% of data to the validation set 
-    if cnt/len(samples) >= 0.70 and cnt/len(samples) < 0.85:
+    if cnt/len(samples) >= 0.75 and cnt/len(samples) < 0.87:
         sample_name = sample + ".mp3"
-        dirs = [directory,"audio_split_2","validation",lang]
+        dirs = [directory,"audio_split_EMI","validation",lang]
         save_path = os.path.join(*dirs)
         try:
             copyfile(os.path.join(recordings,sample_name), os.path.join(save_path,sample_name))
@@ -68,9 +74,9 @@ for sample in samples:
             copyfile(os.path.join(recordings,sample_name), os.path.join(save_path,sample_name))
     
     # Save 15 % of data to the test set 
-    if cnt/len(samples) >= 0.85:
+    if cnt/len(samples) >= 0.87:
         sample_name = sample + ".mp3"
-        dirs = [directory,"audio_split_2","test",lang]
+        dirs = [directory,"audio_split_EMI","test",lang]
         save_path = os.path.join(*dirs)
         try:
             copyfile(os.path.join(recordings,sample_name), os.path.join(save_path,sample_name))
